e95f5b1995202e7a057f9b4a6424f3004835f274
Neelesh <pant20ec064@rmkcet.ac.in>
2024-10-14 10:49:55 +0530

    My E-com project Backend

---
+++ springwebproject/src/main/java/com/example/springwebproject/Startedon30thseptemberApplication.java
@@ -0,0 +1,12 @@
+package com.example.springwebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startedon30thseptemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startedon30thseptemberApplication.class, args);
+	}
+}
---
+++ springwebproject/src/test/java/com/example/springwebproject/Startedon30thseptemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Startedon30thseptemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
